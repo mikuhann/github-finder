a1eb4a15d7ee3232d63512f0d5891263d6387457
Vitalya <ng.kano@gmail.com>
2023-06-20 14:36:36 +0200

    add not found, home, about pages and routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
+import Home from './pages/Home';
+import About from './pages/About';
+import NotFound from './pages/NotFound';
 
 import { APP_TITLE } from './constants/Constants';
+import { ROUTES } from './constants/Routes';
 
 function App() {
   return (
@@ -11,7 +15,11 @@ function App() {
       <div className="flex flex-col justify-between h-screen">
         <Navbar title={APP_TITLE} />
         <main className='container mx-auto px-3 pb-12'>
-          content
+          <Routes>
+            <Route path={ROUTES.home} element={<Home />} />
+            <Route path={ROUTES.about} element={<About />} />
+            <Route path={ROUTES.not_found} element={<NotFound />} />
+          </Routes>
         </main>
         <Footer />
       </div>
