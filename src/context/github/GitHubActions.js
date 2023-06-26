e14fdf716457d772f773b426f1128f1007409894
Vitalya <ng.kano@gmail.com>
2023-06-26 11:36:47 +0200

    add axios, getuserandrepos method, refactor

--- src/context/github/GitHubActions.js
+++ src/context/github/GitHubActions.js
@@ -1,49 +1,33 @@
-import { ROUTES } from '../../constants/Routes';
+import axios from 'axios';
 
 const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
 const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
+
+const github = axios.create({
+  baseURL: GITHUB_URL,
+  headers: {
+    Authorization: `token ${GITHUB_TOKEN}`,
+  },
+});
 
 export const searchUsers = async (search) => {
   const params = new URLSearchParams({
     q: search,
   });
 
-  const res = await fetch(`${GITHUB_URL}/search/users?${params}`, {
-    headers: {
-      Authorization: `token ${GITHUB_TOKEN}`
-    }
-  });
-
-  const { items } = await res.json()
+  const { data: { items } } = await github.get(`/search/users?${params}`);
 
   return items;
 }
 
-export const getUser = async (login) => {
-  const res = await fetch(`${GITHUB_URL}/users/${login}`, {
-    headers: {
-      Authorization: `token ${GITHUB_TOKEN}`,
-    }
-  });
+export const getUserAndRepos = async (login) => {
+  const [user, repos] = await Promise.all([
+    github.get(`/users/${login}`),
+    github.get(`/users/${login}/repos`),
+  ]);
 
-  if (res.status === 404) {
-    return window.location = ROUTES.not_found;
-  }
-
-  return await res.json();
-}
-
-export const getUserRepos = async (login) => {
-  const params = new URLSearchParams({
-    sort: `created`,
-    per_page: 10,
-  });
-
-  const res = await fetch(`${GITHUB_URL}/users/${login}/repos?${params}`, {
-    headers: {
-      Authorization: `token ${GITHUB_TOKEN}`,
-    }
-  });
-
-  return await res.json();
+  return {
+    user: user.data,
+    repos: repos.data,
+  };
 }
